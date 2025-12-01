6cd08f5c50831592d02c0a299613a8d8a82e2d5b
angelolf <angelo.lamure@gmail.com>
2025-12-01 10:09:33 +0100

    maj

--- making_sbatch.py
+++ making_sbatch.py
@@ -40,7 +40,7 @@ def generate_batch(batch_name, codes, device, mult=False, mail=True, log=True, d
 
 		slurm.append(f"\n# Description Partition")
 		slurm.append(f"#SBATCH --partition={params.partition_gpu}_{gpu_device}")
-		# slurm.append(f"#SBATCH --gres=gpu_{gpu_device}")
+		slurm.append(f"#SBATCH --gpus=1")
 		slurm.append(f"#SBATCH --account={params.account}")
 		
 		slurm.append(f"\n# Description de la taches")
